bc94c75262e7da1b61c93e884748a91143b76dad
DESKTOP-5A4MG6P\RafalLietzau-3TP <kremufka69420@gmail.com>
2024-09-30 12:24:33 +0200

    Dodane pierwsze pole tekstowe z id "tytul"

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+import "bootstrap/dist/css/bootstrap.css";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-fluid">
+        <div className="col-6">
+          <label htmlFor="tytul" className="form-label">Tytuł filmu</label>
+          <input type="text" className="form-control" id="tytul" placeholder="" />
+        </div>
     </div>
   );
 }
