0d07a51a50350c955dfbdf79ba4acc40109037ce
DESKTOP-5A4MG6P\RafalLietzau-3TP <kremufka69420@gmail.com>
2024-09-30 12:39:45 +0200

    Dodano przycisk, oraz funkcje ktora pokazuje tytul, oraz rodzaj filmu w konsoli

--- src/App.js
+++ src/App.js
@@ -3,18 +3,17 @@ import './App.css';
 
 function App() {
   
-  const [tytul, setTytul] = useState("");
-  const [rodzaj, setRodzaj] = useState("");
+  function onSubmit(event) {
+    event.preventDefault();
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    console.log(`Tytuł: ${tytul}, Rodzaj: ${rodzaj}`)
+    const tytul = event.target.elements.tytul.value;
+    const rodzaj = event.target.elements.rodzaj.value;
+    console.log(`tytul: ${tytul}, rodzaj: ${rodzaj}`);
   }
 
   return (
     <div className="container-fluid">
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={onSubmit}>
         <div className="col-6">
           <label htmlFor="tytul" className="form-label">Tytuł filmu</label>
           <input type="text" className="form-control" id="tytul" placeholder="" />
@@ -28,7 +27,10 @@ function App() {
           <option value="3">Sensacyjny</option>
           <option value="4">Horror</option>
         </select>  
-        </div>
+        </div><br></br>
+          <button type="submit" className="btn btn-primary">
+            Dodaj
+          </button>
         </form>
     </div>
   );
