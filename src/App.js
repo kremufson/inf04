b6f3294a178a228494b72fdbf31f3ab20e581c47
DESKTOP-5A4MG6P\RafalLietzau-3TP <kremufka69420@gmail.com>
2024-09-30 12:30:01 +0200

    Dodano liste rozwijalną z etykietą o treści "Rodzaj"

--- src/App.js
+++ src/App.js
@@ -4,10 +4,22 @@ import './App.css';
 function App() {
   return (
     <div className="container-fluid">
+      <form>
         <div className="col-6">
           <label htmlFor="tytul" className="form-label">Tytuł filmu</label>
           <input type="text" className="form-control" id="tytul" placeholder="" />
         </div>
+        <div className="col-6">
+        <label htmlFor="inputState" className="form-label">Rodzaj</label>
+        <select id="inputState" className="form-select">
+          <option defaultValue={0}></option>
+          <option value="1">Komedia</option>
+          <option value="2">Obyczajowy</option>
+          <option value="3">Sensacyjny</option>
+          <option value="4">Horror</option>
+        </select>  
+        </div>
+        </form>
     </div>
   );
 }
